30f0aece13ef9697d091caab1d5de754f40a12b0
Adam Grant <adam.jgrant@gmail.com>
2008-10-11 13:54:51 -0700

    Initial merb app generation. Ran 'merb-gen app pool_tracker'

---
+++ config/environments/test.rb
@@ -0,0 +1,7 @@
+Merb.logger.info("Loaded TEST Environment...")
+Merb::Config.use { |c|
+  c[:testing] = true
+  c[:exception_details] = true
+  c[:log_auto_flush ] = true
+  c[:log_level] = :fatal
+}
